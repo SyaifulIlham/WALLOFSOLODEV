a68bd1acb8b862b7d92fa2191b55a6016d6596f2
Xiaomao15 <kagayaku15h@gmail.com>
2026-04-14 14:30:29 +0700

    sudah update auth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const cors = require('cors'); // Pastikan ini ada
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// KONFIGURASI CORS SUPER TERBUKA
+// MIDDLEWARE
 app.use(cors({
-    origin: '*', // Mengizinkan semua frontend mengakses ini
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Mengizinkan aksi CRUD
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
-
 app.use(express.json());
 
-// Pastikan path ini sesuai dengan letak folder routes kamu ya
-// Jika api.js ada di dalam folder src/routes/, ubah jadi require('./src/routes/api')
-const filmRoutes = require('./routes/api'); 
+// ROUTES
+const filmRoutes = require('./routes/api');
+const authRoutes = require('./routes/auth');
+
 app.use('/', filmRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
-<<<<<<< HEAD
   console.log(`Soloflixx server berjalan di http://localhost:${port}`);
-=======
-  console.log(`CineGo server berjalan di http://localhost:${port}`);
->>>>>>> ed3bb03b2a5a86f1cdb6a39c2735f129f05e0355
 });
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-langkah satu lakukan instalasi pada folder fronetend dan backend
-fronend
+langkah satu lakukan instalasi pada folder frontend dan backend
+
+1. frontend
 npm install axios react-router-dom bootstrap react-bootstrap bootstrap-icons 
 
-backend
+2. backend
 npm install express mysql2 cors jsonwebtoken bcrypt
 npm install -D nodemon
